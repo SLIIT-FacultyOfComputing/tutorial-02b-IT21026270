0636dd2e67fbc16c8c86d58c292b0a31dbace235
Nethmi Senarathne <99447197+IT21026270@users.noreply.github.com>
2022-03-11 17:02:49 +0000

    Tutorial2.ex1

--- tute01.cpp
+++ tute01.cpp
@@ -11,20 +11,14 @@ float data1;
 int data2;
 scanf("%f", &data1); --> std::cin >> data1;
 scanf("%d", &data2); --> std::cin >> data2; 
-You already know that printf() in C is std::cout in C++ e.g.
-
-printf("Hello World") --> std::cout << "Hello World";
-2.54cm = 1 inch
-*/
-
-
-#include <stdio.h>
+You already know that printf() in C is std::cout in*/
+#include <iostream>
 int main() 
 {
     float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
+    std::cout<<"Enter a length in cm: ";
+    std::cin>>cm;
     inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+    std::cout<<"Length in inches: "<<inches;
     return 0;
 }  
